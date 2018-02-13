cba689812a3c87b27e178da239c0944482ee6659
Trymunx <trymunx@gmail.com>
2018-02-13 14:21:29 +0000

    Make store global

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import Vuex from 'vuex'
+import store from '@/vuex/store'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
@@ -10,6 +11,7 @@ Vue.use(Vuex)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   template: '<App/>',
   components: { App }
 })
