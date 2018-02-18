8584a3bd5be32e626c3ceeb2c0d87a78a8194582
Trymunx <trymunx@gmail.com>
2018-02-18 00:16:52 +0000

    WIP Add duration tracker

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="todo-item" @mouseover="toggleDeleteButton" @mouseout="toggleDeleteButton">
     <input type="checkbox" @click="toggleComplete(todo)" :checked="todo.done">
-    <div>{{todo.title}}</div>
-    <div class="delete-button" v-show="showDelete" @click="deleteTodo(todo)">X</div>
+    <div class="todo-title" @click="toggleDetails">{{todo.title}}</div>
+    <div class="delete-button" v-if="showDelete" @click="deleteTodo(todo)">X</div>
+    <div class="delete-space" v-else></div>
+    <div class="todo-details" v-if="showDetails">
+      <div class="start-button" v-if="!todo.started && !todo.done" @click="startTodo(todo)">Start</div>
+      <div class="status-bar" v-if="todo.started && !todo.done">Started</div>
+      <div class="status-bar" v-if="todo.started && todo.done">{{elapsedTime}}</div>
+    </div>
   </div>
 </template>
 
 <script type="text/javascript">
 import { addTodo, deleteTodo, toggleComplete } from "@/vuex/actions";
+import Duration from "duration-js";
 
 export default {
   data() {
     return {
-      showDelete: false
+      showDelete: false,
+      showDetails: false
+    }
+  },
+  computed: {
+    elapsedTime() {
+      return new Duration(this.todo.finished - this.todo.started).toString();
     }
   },
   methods: {
@@ -26,6 +39,18 @@ export default {
 
     toggleDeleteButton() {
       this.showDelete = !this.showDelete;
+    },
+
+    toggleDetails() {
+      this.showDetails = !this.showDetails;
+    },
+
+    startTodo(todo) {
+      this.$store.dispatch("startTodo", todo);
+    },
+
+    finishTodo(todo) {
+      this.$store.dispatch("finishTodo", todo);
     }
   },
   props: ["todo"]
@@ -35,13 +60,24 @@ export default {
 <style scoped>
 .todo-item {
   display: flex;
+  flex-wrap: wrap;
   padding: 5px;
   margin: 5px;
   border-radius: 5px;
   font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
 
+.todo-title {
+  flex: 1;
+  min-width: 200px;
+}
+
 .delete-button {
   margin-left: auto;
+  width: 10px;
+}
+
+.delete-space {
+  width: 10px;
 }
 </style>
