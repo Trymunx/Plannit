62e3e3121839dc1cf2bf6c9a4880bb03784fa7d5
Trymunx <trymunx@gmail.com>
2018-04-16 23:11:42 +0100

    Initialise firebase

--- src/main.js
+++ src/main.js
@@ -4,9 +4,21 @@ import Vue from "vue";
 import App from "./App";
 import Vuex from "vuex";
 import store from "@/vuex/store";
+import * as firebase from "firebase";
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyDb3e5NvjYEZT2XkQeVS8mqVwtYJbmIpD0",
+  authDomain: "plannit-todos.firebaseapp.com",
+  databaseURL: "https://plannit-todos.firebaseio.com",
+  projectId: "plannit-todos",
+  storageBucket: "plannit-todos.appspot.com",
+  messagingSenderId: "912835568992"
+};
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
